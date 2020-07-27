ae7e4bbda05b695fd0792bb1ef0bb6453387d2eb
= <harvey.alex01+github@gmail.com>
2020-07-27 15:00:56 +1000

    .

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = normalisePort(process.env.PORT || 3000)
 const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.set("view engine", "ejs");
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // routes
 const home = require('./routes/index')
 const work = require('./routes/work')
 const personal = require('./routes/personal')
-
-
-
 // use routes
 app.use('/', home)
 app.use('/work', work)
@@ -22,4 +41,20 @@ app.use('/personal', personal)
 
 
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
