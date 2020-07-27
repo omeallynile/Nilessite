1bff3ec0a583d49fe7d3b1556317347f792555c5
= <harvey.alex01+github@gmail.com>
2020-07-27 15:01:18 +1000

    .

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-
-const express = require('express')
-const app = express()
-const port = normalisePort(process.env.PORT || 3000)
-const path = require('path')
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
-app.set("view engine", "ejs");
-
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
